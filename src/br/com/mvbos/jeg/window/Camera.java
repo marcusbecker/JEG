b8c3974c43897c13da5b378f3f9a05eb3e1e3d70
Marcus Becker <marcusvbecker@gmail.com>
2014-08-03 11:50:56 -0300

    Cam

--- src/br/com/mvbos/jeg/window/Camera.java
+++ src/br/com/mvbos/jeg/window/Camera.java
@@ -1,5 +1,6 @@
 package br.com.mvbos.jeg.window;
 
+import br.com.mvbos.jeg.element.ElementModel;
 import br.com.mvbos.jeg.engine.Engine;
 
 /**
@@ -12,6 +13,9 @@ public class Camera {
 	private float cpx;
 	private float cpy;
 
+	private int w = 4000;
+	private int h = 3970;
+
 	private static Camera c;
 
 	public static Camera c() {
@@ -20,6 +24,13 @@ public class Camera {
 		}
 
 		return c;
+	}
+
+	public Camera config(int w, int h) {
+		this.w = w;
+		this.h = h;
+		
+		return this;
 	}
 
 	/**
@@ -67,14 +78,50 @@ public class Camera {
 
 	public void rollY(float y) {
 		cpy += y;
-		if (cpy > Engine.getIWindowGame().getCanvasHeight()) {
-			cpy = Engine.getIWindowGame().getCanvasHeight();
+		// if (cpy > Engine.getIWindowGame().getCanvasHeight()) {
+		// cpy = Engine.getIWindowGame().getCanvasHeight();
+		// }
+	}
+
+	public void move(int nx, int ny) {
+		/*
+		 * if (lck == LockType.ALL) { return; }
+		 */
+
+		if (cpx + nx < 0 || cpx + nx > w) {
+			cpx = nx < 0 ? 0 : w;
+		} else {
+			cpx = nx;
 		}
+
+		/*
+		 * if (cpy + ny < 0 || cpy + ny > h) { cpy = cpy + ny < 0 ? 0 : h; }
+		 * else { cpy = cpy + ny; }
+		 */
 	}
 
 	@Override
 	public String toString() {
 		return "Camera [cpx=" + cpx + ", cpy=" + cpy + "]";
+	}
+
+	public void center(ElementModel el) {
+		cpx = el.getPx() - (Engine.getIWindowGame().getCanvasWidth() / 2);
+		cpy = el.getPy() - (Engine.getIWindowGame().getCanvasHeight() / 2);
+
+		if (cpx < 0) {
+			cpx = 0;
+
+		} else if (cpx > (w - Engine.getIWindowGame().getCanvasWidth())) {
+			cpx = w - Engine.getIWindowGame().getCanvasWidth();
+		}
+
+		if (cpy < 0) {
+			cpy = 0;
+
+		} else if (cpy > (h - Engine.getIWindowGame().getCanvasHeight())) {
+			cpy = h - Engine.getIWindowGame().getCanvasHeight();
+		}
 	}
 
 }
